2b31b1583a74e44128a87d827c0c75dc4756454e
jjtey <junjietey@gmail.com>
2022-06-12 23:01:35 +0800

    Working 2D Array for Player number & corresponding rolls.

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
+var noOfPlayers = "";
+var noOfDices = "";
+var gameInstructions = "";
+var playerRolls = [];
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (!noOfPlayers) {
+    if (Number.isInteger(Number(input)) && Math.round(input) >= 2) {
+      noOfPlayers = input;
+      return `Number of Players: ${noOfPlayers}.<br> Please input the number of Dices to roll (at least 2!).`;
+    }
+    return `Please input a number 2 and above!`;
+  }
+  if (!noOfDices) {
+    if (Number.isInteger(Number(input)) && Math.round(input) >= 2) {
+      noOfDices = input;
+      return `Number of Players: ${noOfPlayers}.<br>Number of Dices: ${noOfDices}.`;
+    }
+    return `Please input a number 2 and above!`;
+  }
+  collateRolls(noOfPlayers, noOfDices);
 };
+
+function rollDice() {
+  var diceRoll = Math.floor(Math.random() * 6) + 1;
+  return diceRoll;
+}
+
+function collateRolls(noOfPlayers, noOfDices) {
+  for (var player = 0; player < noOfPlayers; player++) {
+    playerRolls.push([]);
+    for (var roll = 0; roll < noOfDices; roll++) {
+      playerRolls[player][roll] = rollDice();
+    }
+  }
+}
+
+console.log(main(3));
+console.log(main(3));
+console.log(main());
+console.log(playerRolls[0][0]);
+console.log(playerRolls[0][1]);
+console.log(playerRolls[1][0]);
+console.log(playerRolls[1][1]);
