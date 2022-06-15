8083d9f24ea1ad94c76d000425d37a2b08abe773
jjtey <junjietey@gmail.com>
2022-06-16 02:01:59 +0800

    Added winner declaration, pending refactoring

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ var noOfPlayers = "";
 var noOfDice = "";
 var gameInstructions = "";
 const playerRolls = [];
-playerRolls[player] = [];
-var player = "";
-var roll = "";
 var myOutputValue = "";
 var modeChosen = "";
 var winningCriteria = "";
@@ -12,6 +9,7 @@ var playerScore = "";
 
 var main = function (input) {
   if (!noOfPlayers) {
+    //Request for number of players (>= 2)
     if (Number.isInteger(Number(input)) && Math.round(input) >= 2) {
       noOfPlayers = input;
       return `Number of Players: ${noOfPlayers}.<br><br><b> Please input the number of Dice Rolls (at least 2!).<b>`;
@@ -19,20 +17,22 @@ var main = function (input) {
     return `Please input a number 2 and above for the number of Players!`;
   }
   if (!noOfDice) {
+    //Request for number of Dice Rolls (>=2)
     if (Number.isInteger(Number(input)) && Math.round(input) >= 2) {
       noOfDice = input;
-      return `Number of Players: ${noOfPlayers}.<br>Number of Dice Rolls: ${noOfDice}. <br><br><b> Press "Submit" to roll your dice!`;
+      return `Number of Players: ${noOfPlayers}.<br>Number of Dice Rolls: ${noOfDice}. <br><br> Press <b>"Submit"</b> to roll your dice!`;
     }
     return `Please input a number 2 and above for the number of Dice Rolls!`;
   }
   if (myOutputValue.length == 0) {
+    //Generate rolls and request for winning Criteria
     collateRolls(noOfPlayers, noOfDice);
     return (
       myOutputValue +
       `<br>Please type <b>highest</b> or <b>lowest</b>, your rolls will then be arranged to form the highest/lowest possible number!`
     );
   }
-  if (winningCriteria == "") {
+  if (!winningCriteria) {
     if (input == "highest") {
       winningCriteria = "highest";
       modeChosen =
@@ -45,20 +45,24 @@ var main = function (input) {
       modeChosen =
         "Please type <b>highest</b> or <b>lowest</b>, your rolls will then be arranged to form the highest/lowest possible number!";
     }
-    return modeChosen + `<br><br><b>Press "Submit" to find out who won!</b> `;
+    return modeChosen + `<br><br>Press <b>"Submit"</b> to find out who won! `;
   }
-  if (playerScore == "") {
+  if (!playerScore) {
+    //sorts rolls into winning criteria and compares score
     genNumber(noOfPlayers, noOfDice, winningCriteria);
-    return playerScore;
+    return playerScore + `<br><br>Press <b>"Submit"</b> to restart game!`;
   }
+  location.reload(); //refreshes pages on "submit"
 };
 
 function rollDice() {
+  //rolls dice
   var diceRoll = Math.floor(Math.random() * 6) + 1;
   return diceRoll;
 }
 
 function collateRolls(noOfPlayers, noOfDice) {
+  //stores a 2D array with a sub-array of each player's rolls
   for (player = 1; player <= noOfPlayers; player += 1) {
     playerRolls[player] = [];
     for (roll = 0; roll < noOfDice; roll++) {
@@ -68,21 +72,37 @@ function collateRolls(noOfPlayers, noOfDice) {
   }
 }
 
-function genNumber(noOfPlayers, noOfDice, winningCriteria) {
+function genNumber(noOfPlayers, noOfDice, winningCriteria, arrayOfScores) {
+  //generates a number based on winning criteria
+  var arrayOfScores = [];
   for (player = 1; player <= noOfPlayers; player += 1) {
-    playerRolls[player].sort();
+    playerRolls[player].sort(); //sorts ascending
     if (winningCriteria == "lowest") {
-      playerRolls[player].reverse();
+      playerRolls[player].reverse(); //sorts descending
     }
     console.log(playerRolls[player]);
     var sortedRolls = "";
     for (roll = 0; roll < noOfDice; roll += 1) {
       sortedRolls =
-        Number(sortedRolls) + Number(playerRolls[player][roll] * 10 ** roll);
-      console.log(playerRolls[1][0] * 10 ** [0]);
-      console.log(playerRolls[1][1] * 10 ** [1]);
-      console.log(playerRolls[1][2] * 10 ** [2]);
+        Number(sortedRolls) + Number(playerRolls[player][roll] * 10 ** roll); //generates number by allocating digits into ones/tens/hundreds places etc.
     }
     playerScore += `Player ${player}, the ${winningCriteria} number from your Rolls is: ${sortedRolls}! <br>`;
+
+    arrayOfScores.push(sortedRolls);
+  }
+  if (winningCriteria == "highest") {
+    //find highest/lowest number in array of generated numbers and declares winner
+    playerScore += `<br><br> The ${winningCriteria} number is ${Math.max(
+      ...arrayOfScores
+    )}! <br><b> Player ${
+      1 + arrayOfScores.indexOf(Math.max(...arrayOfScores))
+    } wins! </b>`;
+  } else if (winningCriteria == "lowest") {
+    console.log(Math.min(...arrayOfScores));
+    playerScore += `<br><br> The ${winningCriteria} number is ${Math.min(
+      ...arrayOfScores
+    )}! <br><b> Player ${
+      1 + arrayOfScores.indexOf(Math.min(...arrayOfScores))
+    } wins! </b>`;
   }
 }
